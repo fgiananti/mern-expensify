cf1d24b613653511b6fa19b81d0d52383b88d5bb
francesco <fgiananti@gmail.com>
2018-05-18 23:04:05 +0200

    add select

--- client/index.js
+++ client/index.js
@@ -12,9 +12,10 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 450 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 450 }));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 10000 }));
+
 
 const state = store.getState();
 const visibileExpenses = getVisibleExpenses(state.expenses, state.filters);
