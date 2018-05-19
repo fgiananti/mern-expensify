b63765a3881447499a6f18455c971b2c793f28f3
francesco <fgiananti@gmail.com>
2018-05-19 21:02:50 +0200

    add test cases

--- client/tests/actions/expenses.test.js
+++ client/tests/actions/expenses.test.js
@@ -1,9 +1,54 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
-test('should setup remove expense action objext', () => {
+test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '123abc'
+  });
+});
+
+test('should setup edit expense action object', () => {
+  const action = editExpense('123abc', { note: 'New Value'});
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: '123abc',
+    updates: {
+      note: 'New Value'
+    }
+  });
+});
+
+test('should setup add expense action object with provided values', () => {
+  const expenseData = {
+    description: 'Rent',
+    amount: 1950,
+    createdAt: 1000,
+    note: 'This was the last month rent'
+  };
+
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
+
+test('should setup add expense action object with default values', () => {
+  const expenseData = {};
+
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0,
+      id: expect.any(String)
+    }
   });
 });
